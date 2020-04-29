880bc1ccce59efa048ef46d16914bdf10b0b7082
Aviel Ovadiya <aviell1100611@gmail.com>
2020-04-30 01:32:50 +0300

    Lesson 308, upload it because I go to sleep, after signout

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -1,20 +1,73 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface UsernameAvailableResponse {
   available: boolean;
+}
+
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignupResponse {
+  username: string;
+}
+
+interface SignedinResponse {
+  authenticated: boolean;
+  username: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  signedin$ = new BehaviorSubject(false);
+  rootUrl = 'https://api.angular-email.com';
 
   constructor(private http: HttpClient) { }
 
   usernameAvailable(username: string) {
-    return this.http.post<UsernameAvailableResponse>('https://api.angular-email.com/auth/username', {
-      username
-    })
+    return this.http.post<UsernameAvailableResponse>(
+      this.rootUrl + '/auth/username',
+      {
+        username
+      }
+    )
+  }
+
+  signup(credentials: SignupCredentials) {
+    return this.http.post<SignupResponse>(
+      this.rootUrl + '/auth/signup',
+      credentials
+    ).pipe(
+      tap(() => {
+        this.signedin$.next(true);
+      })
+    );
+  }
+
+  checkAuth() {
+    return this.http.get<SignedinResponse>(
+      this.rootUrl + '/auth/signedin'
+    ).pipe(
+      tap(({ authenticated }) => {
+        this.signedin$.next(authenticated);
+      })
+    )
+  }
+
+  signout() {
+    return this.http.post(
+      this.rootUrl + '/auth/signout', {}
+    ).pipe(
+      tap(() => {
+        this.signedin$.next(false);
+      })
+    )
   }
 }
--- emailclient/src/app/auth/signup/signup.component.ts
+++ emailclient/src/app/auth/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -11,29 +12,48 @@ import { UniqueUsername } from '../validators/unique-username';
 export class SignupComponent implements OnInit {
   authForm = new FormGroup({
     username: new FormControl('', [
-      Validators.required, 
-      Validators.minLength(3), 
-      Validators.maxLength(20), 
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
       Validators.pattern(/^[a-z0-9]+$/),
     ], [
       this.uniqueUsername.validate
     ]),
     password: new FormControl('', [
-      Validators.required, 
-      Validators.minLength(4), 
+      Validators.required,
+      Validators.minLength(4),
       Validators.maxLength(20)
     ]),
     passwordConfirmation: new FormControl('', [
-      Validators.required, 
-      Validators.minLength(4), 
+      Validators.required,
+      Validators.minLength(4),
       Validators.maxLength(20)
     ])
   }, { validators: [this.matchPassword.validate] });
 
   constructor(
     private matchPassword: MatchPassword,
-    private uniqueUsername: UniqueUsername
-  ) {}
-  ngOnInit(): void {}
+    private uniqueUsername: UniqueUsername,
+    private authService: AuthService
+  ) { }
+  ngOnInit(): void { }
 
+  onSubmit() {
+    if (this.authForm.invalid) {
+      return;
+    }
+
+    this.authService.signup(this.authForm.value).subscribe({
+      next: (response) => {
+        // Navigate to some other route.
+      },
+      error: (err) => {
+        if(!err.status) {
+          this.authForm.setErrors({ noConnection: true })
+        } else {
+          this.authForm.setErrors({ unKnownError: true })
+        }
+      }
+    });
+  }
 }
